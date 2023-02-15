a9dbb5baa71a24bf9d1db548bb08e54a1b9b8e9a
Jesua Luján <code.masterqro@gmail.com>
2023-02-14 20:02:37 -0600

    simulamoes nuesta pila de ejecución

---
+++ 04.EstructuraDatos/pilas.js
@@ -0,0 +1,56 @@
+//* SIMULAR NUESTRA PROPIA PILA DE JAVASCRIPT
+//* CLASE PILA "APILAR DATOS " ITERANDO INSTANCIAS DE ESA CLASE (OBJETOS)
+
+class Pila {
+    constructor(){
+        this.stack = []; //* stack o pila está vacio
+    }
+    //METODOS DE NUESTRA PILA
+    
+    push(data){ //apilamos
+        this.stack.push(data)
+    }
+
+    pop(){  //desapilar
+        return this.stack.pop()
+    }
+
+    peek(){
+        if(this.stack.length < 1){
+            return null
+        }
+        return this.stack[this.stack.length - 1 ]
+    }
+
+    print(){
+        console.log(this.peek())
+    }
+
+    printStack(){
+        console.log(this.stack)
+    }
+
+    size(){
+        return this.stack.length
+    }
+}
+
+let stack = new Pila()
+
+stack.print()
+console.log(stack.size())
+
+stack.push(1)
+stack.print
+console.log(stack.print())
+stack.push(2)
+stack.print
+console.log(stack.print())
+stack.push(3)
+stack.print
+console.log(stack.print())
+stack.push(4)
+stack.print
+console.log(stack.print())
+console.log(stack.size())
+stack.printStack()
